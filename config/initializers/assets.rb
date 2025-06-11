34f8f9913c2247b6430330b077cd84f6fa19c8db
Aaron Olsen <aaronolsen@Aarons-Mac-mini.local>
2025-06-10 18:28:48 -0700

    Force precompile with SVG fix

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -6,4 +6,5 @@ Rails.application.config.assets.version = "1.0"
 # Add additional assets to the asset load path.
 # Explicitly precompile SVGs in app/assets/images
 Rails.application.config.assets.precompile += %w( facebook.svg instagram.svg tiktok.svg youtube.svg )
+Rails.application.config.assets.paths << Rails.root.join("app/assets/images")
 
